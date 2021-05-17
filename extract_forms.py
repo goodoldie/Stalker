351df030d5b2da3db3fa0b40709c48bc95912171
Pawan <goodoldie1996@gmail.com>
2021-05-17 10:35:32 -0400

    Added web scraping tools

---
+++ extract_forms.py
@@ -0,0 +1,37 @@
+import requests
+from bs4 import BeautifulSoup
+import urlparse
+
+def request(url):
+    try:
+        return requests.get("http://" + url)
+    except requests.exceptions.ConnectionError:
+        pass
+
+
+target_url = "testphp.vulnweb.com/login.php"
+response = request(target_url)
+
+parsed_html = BeautifulSoup(response.content, features="lxml")
+forms_list  = parsed_html.findAll("form")
+
+for form in forms_list:
+    action = form.get("action")
+    post_url = urlparse.urljoin(target_url, action)
+    # print(action)
+    method = form.get("method")
+    # print(method)
+
+    inputs_list = form.findAll("input")
+    post_data = {}
+    for input in inputs_list:
+        input_name = input.get("name")
+        input_type = input.get("type")
+        input_value = input.get("value")
+        if input_type == "text":
+            input_value = "test"
+
+        post_data[input_name] = input_value
+
+    requests.post(post_url, data=post_data)
+
