024e6baf46c126c345b921bb56aff001f757b38c
Divyansha <divyansha217@gmail.com>
2021-05-18 13:46:46 +0530

    Added a port scanner

---
+++ port_scanner.py
@@ -0,0 +1,73 @@
+import argparse
+import socket  # for connecting
+from colorama import init, Fore
+
+from threading import Thread, Lock
+from queue import Queue
+
+init()
+GREEN = Fore.GREEN
+RESET = Fore.RESET
+GRAY = Fore.LIGHTBLACK_EX
+
+N_THREADS = 100
+# thread queue
+q = Queue()
+print_lock = Lock()
+
+
+def port_scan(port):
+    """
+    Scan a port on the global variable `host`
+    """
+    try:
+        s = socket.socket()
+        s.connect((host, port))
+    except:
+        with print_lock:
+            print(f"{GRAY}{host:15}:{port:5} is closed  {RESET}", end='\r')
+    else:
+        with print_lock:
+            print(f"{GREEN}{host:15}:{port:5} is open    {RESET}")
+    finally:
+        s.close()
+
+
+def scan_thread():
+    global q
+    while True:
+        # get the port number from the queue
+        worker = q.get()
+        # scan that port number
+        port_scan(worker)
+        # tells the queue that the scanning for that port
+        # is done
+        q.task_done()
+
+
+def main(host, ports):
+    global q
+    for t in range(N_THREADS):
+        # for each thread, start it
+        t = Thread(target=scan_thread)
+        # when we set daemon to true, that thread will end when the main thread ends
+        t.daemon = True
+        # start the daemon thread
+        t.start()
+
+    for worker in ports:
+        # for each port, put that port into the queue
+        # to start scanning
+        q.put(worker)
+
+    # wait the threads ( port scanners ) to finish
+    q.join()
+
+
+if __name__ == "__main__":
+    host = input("Host IP Address ->")
+    port_range = input("Port Range (x-y) ->")
+    start_port, end_port = port_range.split("-")
+    start_port, end_port = int(start_port), int(end_port)
+    ports = [p for p in range(start_port, end_port)]
+    main(host, ports)
